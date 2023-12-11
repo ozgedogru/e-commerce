d4421a7da51adcccdefef51d196391ba7b32b993
ozgedogru <ozgenurkarademir@gmail.com>
2023-12-12 00:06:45 +0300

    custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      primary: "#23A6F0",
+      secondary: "#252B42",
+      secondtext: "#737373",
+      white: "#FFFFFF",
+    },
     extend: {},
   },
   plugins: [],
