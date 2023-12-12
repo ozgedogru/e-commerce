27a0ab210103cdd132ac216cd30402e9392982c0
ozgedogru <ozgenurkarademir@gmail.com>
2023-12-13 01:06:20 +0300

    homepage css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import Main from "./layouts/Main";
+
 import "./App.css";
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Main />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
