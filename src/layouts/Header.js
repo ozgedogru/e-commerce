3f502dc798b646cf2028a958e01e24fd762a900c
ozgedogru <ozgenurkarademir@gmail.com>
2023-12-19 15:13:41 +0300

    makes header component responsive

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -25,7 +25,7 @@ const Header = () => {
           </div>
         </div>
       </div>
-      <div className="flex justify-center items-center min-h-14">
+      <div className="hidden sm:flex justify-center items-center min-h-14">
         <div className="flex flex-wrap justify-between items-center font-bold w-full px-24">
           <div className="flex p-4 text-2xl font-bold leading-8 tracking-tighter">
             <h3>BrandName</h3>
@@ -72,6 +72,43 @@ const Header = () => {
               </div>
             </div>
           </div>
+        </div>
+      </div>
+      <div className="sm:hidden flex flex-col">
+        <div className="flex justify-between items-center py-8 px-4">
+          <h3 className="font-bold text-2xl">BrandName</h3>
+          <div className="flex items-center text-black font-bold">
+            <div className="flex items-center p-2">
+              <i className="fa-solid fa-search"></i>
+              <p></p>
+            </div>
+            <div className="flex items-center p-2">
+              <i className="fa-solid fa-cart-shopping"></i>
+              <p></p>
+            </div>
+            <div className="flex items-center p-2">
+              <i className="fa-solid fa-bars"></i>
+              <p></p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <nav>
+            <ul className="flex flex-col items-center text-secondtext text-2xl leading-6 tracking-wide gap-8 py-8">
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/shop">Shop</a>
+              </li>
+              <li>
+                <a href="/about">About</a>
+              </li>
+              <li>
+                <a href="/contact">Contact</a>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
     </div>
