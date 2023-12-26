46c35451d81b6667b82dfe0cec92f64f77bc6576
ozgedogru <ozgenurkarademir@gmail.com>
2023-12-27 02:39:26 +0300

    adds redux store

---
+++ src/store/store.js
@@ -0,0 +1,23 @@
+import {
+  combineReducers,
+  legacy_createStore as createStore,
+  applyMiddleware,
+} from "redux";
+import { globalReducer } from "./reducers/globalReducer";
+import { productReducer } from "./reducers/productReducer";
+import { shoppingCartReducer } from "./reducers/shoppingCartReducer";
+import { storeReducer } from "./reducers/storeReducer";
+import { userReducer } from "./reducers/userReducer";
+import { thunk } from "redux-thunk";
+
+export const reducers = combineReducers({
+  globalReducer,
+  productReducer,
+  shoppingCartReducer,
+  storeReducer,
+  userReducer,
+});
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
+export default store;
