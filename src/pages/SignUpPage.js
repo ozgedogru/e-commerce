84cb5b483c33af1e4ea60501703ad6b847a9b5b1
ozgedogru <ozgenurkarademir@gmail.com>
2023-12-27 02:37:46 +0300

    adds login page link as explanation

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -2,11 +2,17 @@ import RegistrationForm from "../components/RegistrationForm";
 
 const SignUpPage = () => {
   return (
-    <div className="flex flex-col justify-center items-center  w-full sm:px-96 px-4 sm:py-20 py-10 ">
-      <div className="flex flex-col w-full sm:p-20 p-8 gap-10 rounded-xl border border-shineblack shadow-md shadow-shineblack">
-        <h2 className="font-bold text-2xl text-shineblack">
-          Registration Form
-        </h2>
+    <div className="flex flex-col justify-center items-center w-full sm:px-48 px-8 sm:py-20 py-10">
+      <div className="flex flex-col sm:w-3/5 w-full sm:p-20 p-8 gap-10 rounded-xl shadow-xl">
+        <h2 className="font-bold text-2xl text-black">Registration Form</h2>
+        <div className="flex flex-col items-start">
+          <p className="text-base text-secondtext">
+            Already have an account?{" "}
+            <a href="/login" className="text-primary">
+              Login here.
+            </a>
+          </p>
+        </div>
         <RegistrationForm />
       </div>
     </div>
