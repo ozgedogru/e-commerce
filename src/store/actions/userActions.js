0a0cb458615bbd67195ed6a88095aabbac6eefc9
ozgedogru <ozgenurkarademir@gmail.com>
2023-12-29 00:23:37 +0300

    change actions and initial state

--- src/store/actions/userActions.js
+++ src/store/actions/userActions.js
@@ -1,9 +1,11 @@
 import { toast } from "react-toastify";
 import { AxiosInstance } from "../../api/axiosInstance";
 
-export const SET_USER = "SET_USER";
+export const SET_LOGGED_IN = "SET_LOGGED_IN";
+export const SET_LOGGED_OUT = "SET_LOGGED_OUT";
 
-export const setUser = (user) => ({ type: SET_USER, payload: user });
+export const setUser = (user) => ({ type: SET_LOGGED_IN, payload: user });
+export const clearUser = () => ({ type: SET_LOGGED_OUT });
 
 export const userLogin = (formData, history) => {
   return (dispatch) => {
@@ -20,5 +22,13 @@ export const userLogin = (formData, history) => {
         console.error("Error:", error);
         toast.error("Error occurred: " + error.response.data.message);
       });
+  };
+};
+
+export const userLogout = () => {
+  return (dispatch) => {
+    dispatch(clearUser());
+    localStorage.removeItem("token");
+    console.log("token temizlendi");
   };
 };
--- src/store/reducers/userReducer.js
+++ src/store/reducers/userReducer.js
@@ -1,12 +1,24 @@
-import { SET_USER } from "../actions/userActions";
+import { SET_LOGGED_IN, SET_LOGGED_OUT } from "../actions/userActions";
 
 const initialState = {
-  user: {},
+  user: {
+    name: null,
+    email: null,
+    token: null,
+    role_id: "3",
+  },
+  isLoggedIn: false,
 };
 export const userReducer = (state = initialState, action) => {
   switch (action.type) {
-    case SET_USER:
-      return { ...state, user: action.payload };
+    case SET_LOGGED_IN:
+      return { ...state, user: action.payload, isLoggedIn: true };
+    case SET_LOGGED_OUT:
+      return {
+        ...state,
+        user: {},
+        isLoggedIn: false,
+      };
     default:
       return state;
   }
