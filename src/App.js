73d80be5c0dad3daeba89c3866e5c097ea2fbb86
ozgedogru <ozgenurkarademir@gmail.com>
2023-12-28 17:33:38 +0300

    rewrite providers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
 import { Flip, ToastContainer } from "react-toastify";
 import Main from "./layouts/Main";
 
 import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
-import { Provider } from "react-redux";
-import store from "./store/store";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Provider store={store}>
-        <Main />
-        <ToastContainer
-          position="top-right"
-          theme="colored"
-          autoClose={4000}
-          transition={Flip}
-        />
-      </Provider>
-    </BrowserRouter>
+    <>
+      <Main />
+      <ToastContainer
+        position="top-right"
+        theme="colored"
+        autoClose={4000}
+        transition={Flip}
+      />
+    </>
   );
 }
 
