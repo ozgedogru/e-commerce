3e99ca6264064c35205fa1cd887c08b662bc1383
ozgedogru <ozgenurkarademir@gmail.com>
2023-12-30 01:43:26 +0300

    autho token

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,10 +1,24 @@
 import axios from "axios";
 
 export const createAxiosInstance = () => {
-  return axios.create({
-    baseURL: "https://workintech-fe-ecommerce.onrender.com/",
-    headers: {},
-  });
+  const token = localStorage.getItem("token");
+
+  return token
+    ? axios.create({
+        baseURL: "https://workintech-fe-ecommerce.onrender.com/",
+        headers: {
+          Authorization: token,
+        },
+      })
+    : axios.create({
+        baseURL: "https://workintech-fe-ecommerce.onrender.com/",
+        headers: {},
+      });
+};
+export let AxiosInstance = createAxiosInstance();
+
+export const renewAxiosInstance = () => {
+  AxiosInstance = createAxiosInstance();
 };
 
-export let AxiosInstance = createAxiosInstance();
+renewAxiosInstance();
