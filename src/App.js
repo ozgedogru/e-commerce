bf7955d0b196ee04bc865d4b95279ac9c7fa32e0
ozgedogru <ozgenurkarademir@gmail.com>
2023-12-30 02:31:16 +0300

    check token info

--- src/App.js
+++ src/App.js
@@ -3,8 +3,40 @@ import Main from "./layouts/Main";
 
 import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
+import { useDispatch } from "react-redux";
+import { AxiosInstance, renewAxiosInstance } from "./api/axiosInstance";
+import { clearUser, setUser } from "./store/actions/userActions";
+import { useEffect } from "react";
+import { setCategories, setRoles } from "./store/actions/globalActions";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // Registration formun role id'lerini fetch et.
+    dispatch(setRoles());
+    dispatch(setCategories());
+
+    //localde token bilgisi var mi?
+    const checkToken = async () => {
+      const token = localStorage.getItem("token");
+
+      if (token) {
+        try {
+          const response = await AxiosInstance.get("/verify");
+          dispatch(setUser(response.data));
+          localStorage.setItem("token", response.data.token);
+          renewAxiosInstance();
+        } catch (error) {
+          dispatch(clearUser());
+          localStorage.removeItem("token");
+          renewAxiosInstance();
+        }
+      }
+    };
+    checkToken();
+  }, [dispatch]);
+
   return (
     <>
       <Main />
