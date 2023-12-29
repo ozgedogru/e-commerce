04696c5192d12355b8b784c4b96468ee30604eae
ozgedogru <ozgenurkarademir@gmail.com>
2023-12-30 02:32:39 +0300

    fetch roles and categories w/thunk

--- src/store/actions/globalActions.js
+++ src/store/actions/globalActions.js
@@ -1,13 +1,32 @@
+import { AxiosInstance } from "../../api/axiosInstance";
+
 export const SET_ROLES = "SET_ROLES";
 export const SET_CATEGORIES = "SET_CATEGORIES";
 export const SET_THEME = "SET_THEME";
 export const SET_LANGUAGE = "SET_LANGUAGE";
 
-export const setRoles = (roles) => ({ type: SET_ROLES, payload: roles });
-export const setCategories = (categories) => ({
-  type: SET_CATEGORIES,
-  payload: categories,
-});
+export const setRoles = () => (dispatch) => {
+  return AxiosInstance.get("/roles")
+    .then((res) => {
+      dispatch({ type: SET_ROLES, payload: res.data });
+    })
+    .catch((error) => console.error("Error:", error));
+};
+
+export const setCategories = () => (dispatch) => {
+  return AxiosInstance.get("/categories")
+    .then((res) => {
+      console.log("fetch categories", res.data);
+      dispatch({
+        type: SET_CATEGORIES,
+        payload: res.data,
+      });
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
+
 export const setTheme = (theme) => ({ type: SET_THEME, payload: theme });
 export const setLanguage = (language) => ({
   type: SET_LANGUAGE,
