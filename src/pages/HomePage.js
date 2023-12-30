35191540810b2556bbdffa770a1dadf964a8fc66
ozgedogru <ozgenurkarademir@gmail.com>
2023-12-30 12:24:41 +0300

    import detail

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,7 +4,7 @@ import ProductCards from "./home-page/ProductCards";
 import Content from "./home-page/Content";
 import Features from "./home-page/Features";
 import Blog from "./home-page/Blog";
-import MainSlider from "../sliders/MainSlider";
+import MainSlider from "../layouts/sliders/MainSlider";
 
 export const HomePage = () => {
   return (
