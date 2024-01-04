972b2c4b0a2d89bc25aa2d90f3da39b587d723d8
ozgedogru <ozgenurkarademir@gmail.com>
2024-01-05 01:52:57 +0300

    add link

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,4 +1,4 @@
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import ellipse1 from "../assets/posts/ellipse1.png";
 import ellipse2 from "../assets/posts/ellipse2.png";
 import ellipse3 from "../assets/posts/ellipse3.png";
@@ -6,55 +6,50 @@ import ellipse4 from "../assets/posts/ellipse4.png";
 import slugify from "slugify";
 
 const ProductCard = ({ product }) => {
-  const history = useHistory();
-  const getProductDetail = () => {
-    const categoryMap = {
-      1: "kadinTisort",
-      2: "kadinAyakkabi",
-      3: "kadinCeket",
-      4: "kadinElbise",
-    };
-    const categoryName = categoryMap[product.category_id];
-    const productNameSlug = slugify(product.name, { lower: true });
-    const url = `/${categoryName}/${product.id}/${productNameSlug}`;
-    history.push(url);
+  const categoryMap = {
+    1: "tisort",
+    2: "ayakkabi",
+    3: "ceket",
+    4: "elbise",
   };
+  const categoryName = categoryMap[product.category_id];
+  const productNameSlug = slugify(product.name, { lower: true });
+  const url = `/${categoryName}/${product.id}/${productNameSlug}`;
 
   const productImg = product.images[0].url;
 
   return (
-    <div
-      onClick={() => getProductDetail()}
-      className="flex flex-col items-center justify-around sm:w-[16rem] sm:min-h-[36rem] w-72 gap-4 pb-8 shadow-md hover:scale-105 transition duration-300 cursor-pointer"
-    >
-      <div className="flex max-h-[20rem]">
-        <img
-          className="w-full object-cover object-center "
-          src={productImg}
-          alt={`card {${product.id}}`}
-        ></img>
-      </div>
-      <h5 className="text-black text-center text-base font-bold">
-        {product.name}
-      </h5>
-      <p className="text-secondtext text-center text-sm font-bold px-1">
-        {product.description}
-      </p>
-      <div className="flex justify-center gap-2">
-        <h5 className="text-pricegrey font-bold text-base ">
-          ${product.price * 2}
+    <Link to={`${url}`}>
+      <div className="flex flex-col items-center justify-around sm:w-[16rem] sm:min-h-[36rem] w-72 gap-4 pb-8 shadow-md hover:scale-105 transition duration-300 cursor-pointer">
+        <div className="flex max-h-[20rem]">
+          <img
+            className="w-full object-cover object-center "
+            src={productImg}
+            alt={`card {${product.id}}`}
+          ></img>
+        </div>
+        <h5 className="text-black text-center text-base font-bold">
+          {product.name}
         </h5>
-        <h5 className="text-pricegreen font-bold text-base">
-          ${product.price}
-        </h5>
+        <p className="text-secondtext text-center text-sm font-bold px-1">
+          {product.description}
+        </p>
+        <div className="flex justify-center gap-2">
+          <h5 className="text-pricegrey font-bold text-base ">
+            ${product.price * 2}
+          </h5>
+          <h5 className="text-pricegreen font-bold text-base">
+            ${product.price}
+          </h5>
+        </div>
+        <div className="flex justify-center gap-1">
+          <img src={ellipse1} alt="color1"></img>
+          <img src={ellipse2} alt="color2"></img>
+          <img src={ellipse3} alt="color3"></img>
+          <img src={ellipse4} alt="color4"></img>
+        </div>
       </div>
-      <div className="flex justify-center gap-1">
-        <img src={ellipse1} alt="color1"></img>
-        <img src={ellipse2} alt="color2"></img>
-        <img src={ellipse3} alt="color3"></img>
-        <img src={ellipse4} alt="color4"></img>
-      </div>
-    </div>
+    </Link>
   );
 };
 
