f8727fe4b1328d0abb0ed33c288399d822c6e56c
ozgedogru <ozgenurkarademir@gmail.com>
2024-06-12 14:53:59 +0200

    css detail

--- src/layouts/dropdowns/UserDropdown.js
+++ src/layouts/dropdowns/UserDropdown.js
@@ -25,43 +25,59 @@ const UserDropdown = () => {
         ) : (
           <div className="flex flex-wrap items-center px-2 gap-1">
             <i className="fa-regular fa-user"></i>
-            <button>
+            <div>
               <Link to="/login">Login / Register</Link>
-            </button>
+            </div>
           </div>
         )}
       </Popover.Button>
-      <Transition
-        as={Fragment}
-        show={isHovered}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
-      >
-        <Popover.Panel
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-          className="absolute z-10 text-center top-6 right-0 w-max bg-white border border-lightgrey2 rounded-md shadow-lg"
+      {isLoggedIn ? (
+        <Transition
+          as={Fragment}
+          show={isHovered}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
         >
-          <div className="flex flex-col items-start gap-1 py-2 px-4">
-            <button className="text-black text-md rounded p-1 py-0 hover:text-shineblack">
-              <Link to="/">User Information</Link>
-            </button>
-            <button className="text-black text-md rounded p-1 py-0 hover:text-shineblack">
-              <Link to="/previous-orders">All Orders</Link>
-            </button>
-            <button className="text-black text-md rounded p-1 py-0 hover:text-shineblack">
-              <Link to="/reviews">Reviews</Link>
-            </button>
-            <button className="text-black text-md rounded p-1 py-0 hover:text-shineblack">
-              <Link to="/discount-coupons">Discount Coupons</Link>
-            </button>
-          </div>
-        </Popover.Panel>
-      </Transition>
+          <Popover.Panel
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            className="absolute z-10 text-center top-6 right-0 w-max bg-white border border-lightgrey2 rounded-md shadow-lg"
+          >
+            <div className="flex flex-col items-start gap-1 py-2 px-4">
+              <Link
+                className="text-black text-md rounded p-1 py-0 hover:text-shineblack"
+                to="/"
+              >
+                User Information
+              </Link>
+              <Link
+                className="text-black text-md rounded p-1 py-0 hover:text-shineblack"
+                to="/previous-orders"
+              >
+                All Orders
+              </Link>
+              <Link
+                className="text-black text-md rounded p-1 py-0 hover:text-shineblack"
+                to="/reviews"
+              >
+                Reviews
+              </Link>
+              <Link
+                className="text-black text-md rounded p-1 py-0 hover:text-shineblack"
+                to="/discount-coupons"
+              >
+                Discount Coupons
+              </Link>
+            </div>
+          </Popover.Panel>
+        </Transition>
+      ) : (
+        <></>
+      )}
     </Popover>
   );
 };
