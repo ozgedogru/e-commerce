bd4cf18ad7beaff602bc978838d743bd1ac3218d
ozgedogru <ozgenurkarademir@gmail.com>
2024-06-12 15:43:09 +0200

    updates page w/user data

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import Main from "./layouts/Main";
 
 import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
-import { useDispatch } from "react-redux";
-import { AxiosInstance, renewAxiosInstance } from "./api/axiosInstance";
+import { useDispatch, useSelector } from "react-redux";
 import { clearUser, setUser } from "./store/actions/userActions";
 import { useEffect } from "react";
 import { setCategories, setRoles } from "./store/actions/globalActions";
 import { fetchBestSellers } from "./store/actions/productActions";
+import { AxiosInstance, renewAxiosInstance } from "./api/axiosInstance";
 
 function App() {
   const dispatch = useDispatch();
+
+  const { isLoggedIn } = useSelector((state) => state.userReducer);
 
   useEffect(() => {
     dispatch(setRoles());
@@ -19,14 +21,14 @@ function App() {
     dispatch(fetchBestSellers(null));
 
     //localde token bilgisi var mi?
-    const checkToken = async () => {
+    const checkTokenValidity = async () => {
       const token = localStorage.getItem("token");
 
       if (token) {
         try {
           const response = await AxiosInstance.get("/verify");
           dispatch(setUser(response.data));
-          localStorage.setItem("token", response.data.token);
+
           renewAxiosInstance();
         } catch (error) {
           dispatch(clearUser());
@@ -35,8 +37,8 @@ function App() {
         }
       }
     };
-    checkToken();
-  }, [dispatch]);
+    checkTokenValidity();
+  }, [dispatch, isLoggedIn]);
 
   return (
     <>
