83abea9f3313bc32873791f8726725bd5b8c90a0
ozgedogru <ozgenurkarademir@gmail.com>
2024-07-29 10:39:05 +0200

    ngrok

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,17 +1,20 @@
 import axios from "axios";
 
+const BASE_URL =
+  "https://191b-2001-4c4e-2494-d000-9c78-6753-1c5a-73ef.ngrok-free.app";
+
 export const createAxiosInstance = () => {
   const token = localStorage.getItem("token");
 
   return token
     ? axios.create({
-        baseURL: "http://localhost:8080/",
+        baseURL: BASE_URL,
         headers: {
           Authorization: `Bearer ${token}`,
         },
       })
     : axios.create({
-        baseURL: "http://localhost:8080/",
+        baseURL: BASE_URL,
         headers: {},
       });
 };
