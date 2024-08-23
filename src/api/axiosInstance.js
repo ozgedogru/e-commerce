d0a70349cd9b0b9b0f145b27a1ba29901cef3172
ozgedogru <ozgenurkarademir@gmail.com>
2024-08-23 14:36:33 +0200

    update

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,17 +1,19 @@
 import axios from "axios";
 
+const BASE_URL = "http://localhost:8080/";
+
 export const createAxiosInstance = () => {
   const token = localStorage.getItem("token");
 
   return token
     ? axios.create({
-        baseURL: "http://localhost:8080/",
+        baseURL: BASE_URL,
         headers: {
           Authorization: `Bearer ${token}`,
         },
       })
     : axios.create({
-        baseURL: "http://localhost:8080/",
+        baseURL: BASE_URL,
         headers: {},
       });
 };
